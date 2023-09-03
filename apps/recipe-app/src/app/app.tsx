8b47f183cdd1ce45dc33afe189747d2d06a3dd3d
Su Anli <su.anli@anlitz.com>
2023-09-03 14:53:59 +0800

    feat: add react navigation

--- apps/recipe-app/src/app/app.tsx
+++ apps/recipe-app/src/app/app.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import { WelcomePage } from '../pages'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator()
 
 export const App = () => {
-  return <WelcomePage />
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="WelcomePage"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="WelcomePage" component={WelcomePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
